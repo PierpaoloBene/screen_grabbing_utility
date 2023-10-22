23803d36bd93a9a4d7a9d31dd31163a6cb6f9b31
Pepposo <peppobene@gmail.com>
2023-10-22 20:14:36 +0200

    Finestra Iniziale creata

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
+use core::fmt;
+use std::fmt::Display;
 
+use eframe::{
+    egui::{self, Layout, RichText},
+    emath::Align,
+};
+
+
+#[derive(PartialEq,Debug)]
+enum ModeOptions {
+    Rectangle,
+    FullScreen ,
+}
+
+fn main() -> Result<(), eframe::Error> {
+    let mut selected:ModeOptions = ModeOptions::Rectangle;
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(640.0, 480.0)),
+        ..Default::default()
+    };
+
+    eframe::run_simple_native("Screen Grabbing Utility", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.add_space(310.0); // da modificare
+                if ui.add_sized([50.,50.], egui::Button::new(RichText::new("+").size(30.0))).clicked(){
+                    println!("caccona");
+                }
+
+                egui::ComboBox::from_id_source("my_combobox").width(200.0)
+                .selected_text(format!("{:?}", selected))
+                .show_ui(ui, |ui|{
+                    ui.selectable_value(&mut selected,ModeOptions::Rectangle, RichText::new("Rectangle").size(30.0));
+                    ui.selectable_value(&mut selected, ModeOptions::FullScreen , RichText::new("FullScreen").size(30.0));
+                });
+            });
+        });
+    })
 }
